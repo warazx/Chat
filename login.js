22c9faf4d02cbdbab6ebc04c9c50fc7b9bd187ff
ASA <asa.kwarnmark@gmail.com>
2017-05-04 15:19:42 +0200

    code review: corrected spelling errors

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ var app = angular.module('app', []);
 
 app.controller('loginController', function($scope) {
     $scope.userLogin = function(login){
-        //Check that username is not already in use ny an other user.
+        //Check that username is not already in use by another user.
         var users = ["erika","åsa","tobbe"];
         if(users.indexOf($scope.login.username) !== -1) {
             alert('user already exists!');

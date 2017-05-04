5b3b947bf1334f021c18b9e94f1bb27be7b24d4b
tobiashillen <tobias.hillen@gmail.com>
2017-05-04 11:12:52 +0200

    Added isPrivateMessage-bool to chatmessage-model

--- scripts/temp-chatmessage-object.js
+++ scripts/temp-chatmessage-object.js
@@ -6,12 +6,13 @@ var chatMessage = {
   id : 34634, //MessageID
   text : "",
   date : Date.now(),
+  isPrivateMessage : true,
   sender : 15, //User-ID
   receiver : 16 //UserID / ChatRoomID
-}
+};
 
 var chatLog = {
   id : 2525, //ChatRoomID
   messages : [], //chatMessage's
   receivers : [] //UserID's / ChatRoomID
-}
+};

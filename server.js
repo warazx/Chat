fee1e514d10d1cec1faf4a61b947bfcefd9740a1
scottckr <scottckr@hotmail.com>
2017-05-08 11:58:51 +0200

    Setup some more MongoDB stuff.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = 3000;
 var db;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongo.connect('mongodb://shutapp:shutapp123@ds133981.mlab.com:33981/shutapp', function(err, database) {
     if (err) {
@@ -12,6 +15,16 @@ mongo.connect('mongodb://shutapp:shutapp123@ds133981.mlab.com:33981/shutapp', fu
     }
     db = database;
 });
+
+/*app.post('/messages', function(req, res) {
+    db.collection('users').save(req.body, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Saved to database.");
+        res.redirect('/');
+    })
+})*/
 
 app.use(express.static(__dirname + '/public'));
 

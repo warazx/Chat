203e1a7456a1e982de13a5f65440aa58e00e14b5
scottckr <scottckr@hotmail.com>
2017-05-08 11:37:01 +0200

    Set up MongoDB connection.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
 var port = 3000;
+var db;
+
+mongo.connect('mongodb://shutapp:shutapp123@ds133981.mlab.com:33981/shutapp', function(err, database) {
+    if (err) {
+        console.log(err);
+    }
+    db = database;
+});
 
 app.use(express.static(__dirname + '/public'));
 

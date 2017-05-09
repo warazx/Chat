978611b0a3054dcee9b5a2eb5cdd3663e18e6a9d
tobiashillen <tobias.hillen@gmail.com>
2017-05-09 09:45:02 +0200

    Minor changes to listening function

--- server.js
+++ server.js
@@ -52,8 +52,6 @@ io.on('connection', function(socket){
   });
 });
 
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('Listening on: ' + port);
 });
-

134e654353785464122b58c74f326faad5ffc113
tobiashillen <tobias.hillen@gmail.com>
2017-05-24 14:21:57 +0200

    Server now handles sockets for private messages instead of client.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -328,11 +328,9 @@ app.controller('MessagesController', function ($scope, $rootScope, $http, $locat
                 "recipientId": $rootScope.privateRecipient.id,
                 "recipientName": $rootScope.privateRecipient.name
             };
-            //Send a direct private message. socket.io needs the socketId to know where to send it.
-            newPrivateMessage.socketId = $rootScope.privateRecipient.socketId;
+            //Send a direct private message. 
             mySocket.emit('private message', newPrivateMessage);
             //Post the message to the database
-            newPrivateMessage.socketId = undefined;
             $http.post('/private-messages', newPrivateMessage);
         };
 

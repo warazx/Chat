7f0cecfdf8fcc388e0a9da79ff66d70bd7775c13
ASA <asa.kwarnmark@gmail.com>
2017-06-02 10:27:37 +0200

    started working on removing online conversation partners from offline list

--- shutAppMobile/www/js/app.js
+++ shutAppMobile/www/js/app.js
@@ -34,45 +34,98 @@ app.factory('socket', function(socketFactory) {
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('login', {
-      url: '/login',
-      templateUrl: 'partials/login.html',
-      controller: 'LoginController'
-    })
-    .state('signup', {
-      url: '/signup',
-      templateUrl: 'partials/signup.html',
-      controller: 'SignupController'
-    })
-    .state('messages', {
-        url: '/messages',
-        templateUrl: 'partials/messages-and-menu.html',
-        /*
-        views: {
-            'menuContent': {
-                templateUrl: 'partials/sidemenu.html'
-            },
-            'messagesContent': {
-                templateUrl: 'partials/messages.html'
-            }
-        },
-        */
-        controller: 'MessagesController'
-    })
-    .state('settings', {
-      url: '/settings',
-      templateUrl: 'partials/settings.html',
-      controller: 'SettingsController'
-    });
+  .state('login', {
+    url: '/login',
+    templateUrl: 'partials/login.html',
+    controller: 'LoginController'
+  })
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'partials/signup.html',
+    controller: 'SignupController'
+  })
+  .state('messages', {
+    url: '/messages',
+    templateUrl: 'partials/messages-and-menu.html',
+    controller: 'MessagesController'
+  })
+  .state('settings', {
+    url: '/settings',
+    templateUrl: 'partials/settings.html',
+    controller: 'SettingsController'
+  });
   $urlRouterProvider.otherwise('/login');
 });
 
-app.controller('LoginController', function ($rootScope, messageManager) {
-  $rootScope.jepp = "Login";
+app.controller('LoginController', function ($rootScope, $scope, $location, userManager) {
+    $rootScope.currentView = "Login";
+
+    //Needed on scope before login credentials are entered by user.
+    $scope.login = {};
+
+    //Message to show in toast. Not implementet
+    $scope.errorMessage = "";
+
+    $scope.userLogin = function () {
+        if ($scope.login.username === undefined || $scope.login.password === undefined) {
+            console.log('Invalid logininformation.');
+            $scope.errorMessage = "Felaktiga inloggningsuppgifter.";
+        } else {
+            userManager.login($scope.login.username, $scope.login.password).then(function (res) {
+                console.log('Login successful.');
+                //Might use this....
+                $rootScope.isPrivate = false;
+                $rootScope.user = {
+                    id: res.data._id,
+                    name: res.data.username
+                };
+                $location.path(res.data.redirect); //Redirects to /messages.
+            }, function (res) {
+                console.log('Login failed on server.');
+                $scope.errorMessage = "Felaktiga inloggningsuppgifter.";
+            });
+        }
+        $rootScope.successMessage = "";
+    };
 });
 
-app.controller('SignupController', function ($rootScope, messageManager) {
-  $rootScope.jepp = "Signup";
+app.controller('SignupController', function ($location, $scope, $rootScope, userManager) {
+  $scope.errorMessage = "";
+  $rootScope.successMessage = "";
+  $scope.userSignup = function (signup) {
+    if (signup === undefined || signup.email === undefined || signup.username === undefined || signup.password === undefined || signup.passwordagain === undefined) {
+      var message = "";
+      if (signup.username === undefined) message += "Du måste välja ett användarnamn som innehåller minst tre tecken och max 20 tecken." +
+      "\nDu kan inte använda speciella tecken, endast siffror och bokstäver(a-z).";
+      if (signup.email === undefined) message += "\nFelaktig emailadress.";
+      if (signup.password === undefined) message += "\nDu måste välja ett lösenord som innehåller minst sex tecken och max 50 tecken.";
+      $scope.errorMessage = message;
+    } else if(signup.password !== signup.passwordagain) {
+      $scope.errorMessage = "Du skrev in lösenordet olika i de två fälten.";
+    } else {
+      userManager.signupuser({
+        username: signup.username,
+        email: signup.email,
+        password: signup.password
+      }).then(function (res) { //Successful codes 100-399.
+        console.log("Signup OK. Redirecting to login.");
+        $rootScope.successMessage = "Användare registrerad.";
+        $location.path(res.data.redirect);
+      }, function (res) { //Failed codes 400-599+?
+        console.log("Signup failed.");
+        switch (res.data.reason) {
+          case "username":
+            $scope.errorMessage = "Användarnamnet är upptaget.";
+            break;
+          case "email":
+            $scope.errorMessage = "Emailadressen är redan registrerad.";
+            break;
+          default:
+            $scope.errorMessage = "Det blev lite fel!";
+        }
+      });
+    }
+  };
 });
 
 app.controller('MessagesController', function ($rootScope, $scope, $ionicScrollDelegate, $ionicSideMenuDelegate, messageManager) {
@@ -92,19 +145,44 @@ app.controller('MessagesController', function ($rootScope, $scope, $ionicScrollD
   };
 });
 
-app.controller('SettingsController', function ($rootScope, messageManager) {
-    $rootScope.jepp = "Settings";
-});
-
-app.controller('LeftSideController', function ($rootScope, $scope, messageManager) {
-    /*
-    $scope.chatrooms = ["General", "Random", "FUN!!!"];
-    */
-    
+app.controller('LeftSideController', function ($rootScope, $scope, $location, messageManager, socket) {
+    socket.emit('connected', $rootScope.user);
     messageManager.getChatrooms().then(function (response) {
         $scope.chatrooms = response.data;
     });
-    
+	messageManager.getConversations($rootScope.user.id).then(function (response) {
+		  $rootScope.conversations = $rootScope.offlineConversations = response.data;
+	});
+  socket.on('active users', function (arr) {
+      $rootScope.activeUsers = arr;
+      var activeUserIds = $rootScope.activeUsers.map(x=>x.id);
+      for(var i=0; i<$rootScope.conversations.length; i++) {
+        if(activeUserIds.includes($rootScope.conversations[i].id)) {
+
+        }
+      }
+  });
+
+  $rootScope.changeRecipient = function changeRecipient(recipientId) {
+        //$rootScope.isPrivate = true;
+        //$rootScope.selected = index;
+        $rootScope.privateRecipient = this.privateRoom;
+        /*
+        if ($rootScope.newMessages.includes(this.privateRoom.id)) {
+            $rootScope.newMessages.splice($rootScope.newMessages.indexOf(this.privateRoom.id), 1);
+        }
+        */
+        if (!$rootScope.user) {
+            console.log("User not logged in! Redirecting to login.");
+            $location.path('/');
+        } else {
+            $location.path('/messages');
+            messageManager.getPrivateMessages($rootScope.user.id, $rootScope.privateRecipient.id).then(function(res) {
+                $rootScope.messages = res.data;
+            });
+        }
+    };
+
     /*
     //get list of users with which we have had a conversation
     messageManager.getConversations($rootScope.user.id).then(function(res) {
@@ -171,9 +249,30 @@ app.controller('LeftSideController', function ($rootScope, $scope, messageManage
 })
 */
 
+app.controller('SettingsController', function ($location, $scope, $rootScope, userManager) {
+  $scope.errorMessage = "";
 
+  $scope.changeUsername = function(newUsername) {
+    if(newUsername) {
+        userManager.updateUsername({
+            "id": $rootScope.user.id,
+            "username": newUsername
+        }).then(function () {
+            $rootScope.user.name = newUsername;
+            $scope.errorMessage = "Användarnamnet har ändrats.";
+        }, function () {
+            $scope.errorMessage = "Användarnamnet gick inte att ändra.";
+        });
+        //TODO: Turn on again after sockets work
+        //mySocket.emit('change username', {"id": $rootScope.user.id, "newUserName": newUsername});
+    } else {
+        $scope.errorMessage = "Du måste välja ett användarnamn som innehåller minst tre tecken och max 20 tecken." +
+            "\nDu kan inte använda speciella tecken, endast siffror och bokstäver(a-z).";
+    }
+  };
 
-app.controller('SettingsController', function ($rootScope, messageManager) {
-  $rootScope.jepp = "Settings";
+  $scope.logout = function() {
+    $rootScope.user = {};
+    $location.path('/login');
+  };
 });
-
